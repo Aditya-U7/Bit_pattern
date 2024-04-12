cddf554a30f0cd4adc7fc28adc0c6b013482cd02
Aditya Upadhye <66748266+Aditya-U7@users.noreply.github.com>
2024-04-12 16:08:42 +0530

    Added the condition to check a character.
    If a character in the user provided string requires more than 8 bits for binary representation, then the program will terminate.

--- text_to_bit_art.cpp
+++ text_to_bit_art.cpp
@@ -9,16 +9,25 @@ full block character (Unicode hex value: 2588), and the 0's are represented usin
 
 
 #include <iostream>
+#include <cstdlib>
 
 
-signed int decimal_to_binary(signed int dec_num)
+unsigned int decimal_to_binary(unsigned int dec_num)   //char chr)
 {
 
-	signed int bin_num = 0;
+	static uint8_t count = 1;
 
-	signed int decimal_place = 1;
+	if (dec_num > 127)
+	{
 
-	signed int tmp = 0;  
+		std::cout << "This character at position " << +count << " requires more than 8 bits " << std::endl;
+		exit(1);
+	}
+	unsigned int bin_num = 0;
+
+	unsigned int decimal_place = 1;
+
+	unsigned int tmp = 0;  
 
 	while (dec_num)
 	{
@@ -30,6 +39,7 @@ signed int decimal_to_binary(signed int dec_num)
 
 		decimal_place *= 10;
 	} 
+	count++;
 
 	return bin_num;
 }
@@ -37,31 +47,34 @@ signed int decimal_to_binary(signed int dec_num)
 
 std::string generate_the_binary(std::string user_string)
 {
-        std::string bin_num = ""; 
+	std::string bin_num = ""; 
 	std::string bin_num_space = "";
-	signed int bit_repr = 0;
-        std::string tmp = "";
-	
-	for (auto character: user_string)
-        {
+	unsigned int bit_repr = 0;
+	std::string tmp = "";
+
+	for (auto& character: user_string)
+	{
 		bit_repr = decimal_to_binary(character);
-                 
+
 		tmp = std::to_string(bit_repr);
+
 		if (tmp.length() < 8)
 		{
-		    short diff = 8 - tmp.length();
-		    while (diff)
-		    {
-			tmp = "0" + tmp;
-			diff--;
-	            }
-	         } 
+			short diff = 8 - tmp.length();
+			while (diff)
+			{
+				tmp = "0" + tmp;
+				diff--;
+			}
+		}
+
 		bin_num += tmp;
-                bin_num_space += tmp + " ";
-        }
-        
-        std::cout << "\nThe binary representation using 8-bit:\n" << bin_num_space;
-	
+		bin_num_space += tmp + " ";
+
+	}
+
+	std::cout << "\nThe binary representation using 8-bit:\n" << bin_num_space;
+
 	return bin_num;
 }
 
@@ -75,18 +88,18 @@ int main()
 
 	std::string bin_num = generate_the_binary(user_input);
 	std::cout << "\n";
-	short count = 0;
+	short char_count = 0;
 	for (auto character : bin_num)
 	{
-		if (count % 8 == 0)
+		if (char_count % 8 == 0)
 			std::cout << "\n";
 		if (character == '1')
 			std::cout << "\u2588";
 		else
 			std::cout << " ";
-		++count;
+		++char_count;
 	}
-	
+
 	std::cout << "\n\n";
 	return 0;
 
